453be5a3086f77c5627b80dd6893eef67c3f2692
Olivia Harris <livhvrris@Olivias-MacBook-Pro.local>
2016-10-26 20:17:57 -0700

    Started JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+var questions = [
+
+  {
+    question: "Question 1"
+    answerChoices: ["1", "2", "3", "4"]
+    correctAnswer: 0
+  },
+  {
+    question: "Question 2"
+    answerChoices: ["1", "2", "3", "4"]
+    correctAnswer: 0
+  },
+  {
+    question: "Question 3"
+    answerChoices: ["1", "2", "3", "4"]
+    correctAnswer: 1
+  }
+  {
+    question: "Question 4"
+    answerChoices: ["1", "2", "3", "4"]
+    correctAnswer: 0
+  }
+  {
+    question: "Question 5"
+    answerChoices: ["1", "2", "3", "4"]
+    correctAnswer: 1
+  }
+
+];
+
