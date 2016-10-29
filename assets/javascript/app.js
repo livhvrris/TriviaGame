6dba839c5c0cf9199522d1b5f5776469e81c0565
Olivia Harris <livhvrris@Olivias-MacBook-Pro.local>
2016-10-29 15:57:49 -0700

    still working on logging and validating user answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,7 +28,7 @@ var triviaQuestions = [
 
 ];
 
-var userAnswer;
+var userAnswer = [];
 
 var userCorrectAnswers = 0;
 var userIncorrectAnswers = 0;
@@ -49,6 +49,7 @@ function preGame(){
   $('.endOfGame').hide();
 
   $('#gameStartButton').on('click', startGame);
+  // add event lister for answer on click here... ???
 
 }
 
@@ -102,49 +103,33 @@ function triviaGameStart() {
   for (var i = 0; i < triviaQuestions[i].answerChoices.length; i++){
 
     // NEED TO IMPLEMENT USER ANSWER VALIDATION
+          // PUSH User answrs to an array -- when time is up, vaidate array of userAnswers against Correct answers ???
+          // and put event listener for user click in separate function ???
     // NEED TO INDICATE IF BUTTON IS CLICKED OR CHANGE TO RADIO BUTTONS
-    // var userAnswer = 0;
-    // console.log(userAnswer)
-    // $("#answerOne").on("click", userAnswerValidation)
+      // Also, think of the user answer array -- if a user clicks multiple buttons, we don't want all of them to push... ???
 
+    userAnswer = $("#answerOne").on ("click", userAnswerValidation)
 
-    $("#questionOne").html(triviaQuestions[0].question);
-    $('#answerOne').append('<button class="answerButton">' + triviaQuestions[0].answerChoices[i] + '</button>')
+      $("#questionOne").html(triviaQuestions[0].question);
+      $('#answerOne').append('<button class="answerButton">' + triviaQuestions[0].answerChoices[i] + '</button>')
 
-    $("#questionTwo").html(triviaQuestions[1].question);
-    $('#answerTwo').append('<button class="answerButton">' + triviaQuestions[1].answerChoices[i] + '</button>')
+      $("#questionTwo").html(triviaQuestions[1].question);
+      $('#answerTwo').append('<button class="answerButton">' + triviaQuestions[1].answerChoices[i] + '</button>')
 
-    $("#questionThree").html(triviaQuestions[2].question);
-    $('#answerThree').append('<button class="answerButton">' + triviaQuestions[2].answerChoices[i] + '</button>')
+      $("#questionThree").html(triviaQuestions[2].question);
+      $('#answerThree').append('<button class="answerButton">' + triviaQuestions[2].answerChoices[i] + '</button>')
 
-    $("#questionFour").html(triviaQuestions[3].question);
-    $('#answerFour').append('<button class="answerButton">' + triviaQuestions[3].answerChoices[i] + '</button>')
+      $("#questionFour").html(triviaQuestions[3].question);
+      $('#answerFour').append('<button class="answerButton">' + triviaQuestions[3].answerChoices[i] + '</button>')
 
-    $("#questionFive").html(triviaQuestions[4].question);
-    $('#answerFive').append('<button class="answerButton">' + triviaQuestions[4].answerChoices[i] + '</button>')
+      $("#questionFive").html(triviaQuestions[4].question);
+      $('#answerFive').append('<button class="answerButton">' + triviaQuestions[4].answerChoices[i] + '</button>')
 
   };
-}
+
+};
 
 
-function userAnswerValidation() {
-
-  if (userAnswer === triviaQuestions[0].correctAnswer) {
-    userCorrectAnswers++
-    console.log(userCorrectAnswers);
-  } 
-
-  else if (userAnswer !== triviaQuestions[0].correctAnswer) {
-    userIncorrectAnswers++
-    console.log(userIncorrectAnswers);
-  }
-
-  else {
-    userUnanswered++
-    console.log(userUnanswered);
-  }
-
-}
 
 function endGame() {
 
@@ -157,3 +142,38 @@ function endGame() {
   $('#unansweredCount').html('<p>Unanswered: ' + userUnanswered + '</p>')
 
 }
+
+// ______________________________________________
+
+function userAnswerValidation() {
+  if (userAnswer === triviaQuestions[i].correctAnswer) {
+    userCorrectAnswers++
+    console.log(userCorrectAnswers);
+  } 
+
+  else if (userAnswer !== triviaQuestions[i].correctAnswer) {
+    userIncorrectAnswers++
+    console.log(userIncorrectAnswers);
+  }
+
+  else {
+    userUnanswered++
+    console.log(userUnanswered);
+  }
+}
+
+    // $(".answer").on ("click", function(){
+
+    //   if ($(this).data("id") === triviaQuestions[i].correctAnswer) {
+    //     userCorrectAnswers++;
+    //     console.log(userCorrectAnswers);
+    //   } else if ($(this).data("id") === triviaQuestions[i].correctAnswer) {
+    //     userIncorrectAnswers++;
+    //     console.log(userIncorrectAnswers);  
+    //   } 
+    //   else {
+    //     userUnanswered++
+    //     console.log(userUnanswered);
+    //   }
+      
+    // });
